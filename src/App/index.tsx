6102d6972e5732daa367f3ece1c16f361381a269
Carlos Henrique da S. Moreira <carlos.moreira.tech@gmail.com>
2020-07-13 00:40:44 -0300

    Rename project folders and files

--- src/App/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const App: React.FunctionComponent= () => <h1>Test React!!!</h1>;
-
-export default App;
---
+++ src/app/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '../components/button';
+
+const App = () => (
+    <Button>Teste</Button>
+);
+
+export default App;
