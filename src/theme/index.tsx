acdf92fe489d891c84b576b95ba069aa2acf813e
Carlos Henrique da S. Moreira <carlos.moreira.tech@gmail.com>
2020-07-13 00:41:13 -0300

    Creates theme provider

---
+++ src/theme/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+
+import { defaultTheme } from './themes';
+
+type Props  = {
+  theme?: DefaultTheme,
+  children: React.ReactChild
+}
+
+const Theme = ({ children, theme = defaultTheme }: Props) => (
+  <ThemeProvider theme={theme}>
+    {children}
+  </ThemeProvider>
+);
+
+export default Theme;
