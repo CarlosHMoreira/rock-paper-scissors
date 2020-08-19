b34724c591abde3529956149c8617d5698cfaffc
Carlos Henrique da S. Moreira <carlos.moreira.tech@gmail.com>
2020-08-18 23:22:11 -0300

    Adjusts global styles

--- src/assets/styles/global.tsx
+++ src/assets/styles/global.tsx
@@ -6,7 +6,7 @@ import BarlowSemiCondensedSemiBold from '../fonts/BarlowSemiCondensedSemiBold.tt
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
-    font-family: 'BarlowSemiCondensedBold';
+    font-family: 'BarlowSemiCondensed';
     src: local('BarlowSemiCondensed-Bold'), local('BarlowSemiCondensedBold'),
     url(${BarlowSemiCondensedBold}) format('truetype');
     font-weight: 700;
@@ -14,15 +14,14 @@ const GlobalStyle = createGlobalStyle`
   }
 
   @font-face {
-    font-family: 'BarlowSemiCondensedSemiBold';
+    font-family: 'BarlowSemiCondensed';
     src: local('BarlowSemiCondensed-SemiBold'), local('BarlowSemiCondensedSemiBold'),
     url(${BarlowSemiCondensedSemiBold}) format('truetype');
-    font-weight: 600;
     font-style: normal;
   }
 
   body {
-    font-family: BarlowSemiCondensedBold;
+    font-family: BarlowSemiCondensed;
     background: radial-gradient(circle, ${({ theme: { colors: { backgroundGradient } } }) => backgroundGradient.from } 0%, ${({ theme: { colors: { backgroundGradient } } }) => backgroundGradient.to } 50%);
     background-position: 0 80%;
     background-size: 100% 200%;
