8c621a66930ae61a06d8faa0e5aa3324beb323ae
Carlos Henrique da S. Moreira <carlos.moreira.tech@gmail.com>
2020-08-18 23:48:38 -0300

    Adds global style to storybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,5 +3,11 @@ import { addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 
 import { defaultTheme } from '../src/theme/themes'
+import GlobalStyle from '../src/assets/styles/global';
 
-addDecorator(storyFn => <ThemeProvider theme={defaultTheme}>{storyFn()}</ThemeProvider>);
+addDecorator(storyFn => (
+  <ThemeProvider theme={defaultTheme}>
+    <GlobalStyle />
+    { storyFn() }
+  </ThemeProvider>
+));
